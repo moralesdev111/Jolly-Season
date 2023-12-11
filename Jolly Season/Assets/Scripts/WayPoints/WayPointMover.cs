703494b1843b732f62e3c6006f957ffb3decbe94
Guillermo Mora <moralesdev111@gmail.com>
2023-12-11 13:57:20 +0100

    day6TrainSystemBUGFIXED

--- Jolly Season/Assets/Scripts/WayPoints/WayPointMover.cs
+++ Jolly Season/Assets/Scripts/WayPoints/WayPointMover.cs
@@ -2,54 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WayPointMover : MonoBehaviour
+public class WaypointMover : MonoBehaviour
 {
-    [SerializeField] Waypoints waypoints;
-
+    [SerializeField] private Waypoints waypoints; // reference to waypoint system
     [Header("Movement Settings")]
-    [SerializeField] float moveSpeed = 15f;
-    [SerializeField] float distanceThreshold = 0.1f;
-    [SerializeField] private float rotateSpeed = 5f; // Adjusted the rotateSpeed
-    Transform currentWaypoint;
+    [SerializeField ]private float movementSpeed = 40f;   
+    [SerializeField] private float rotationSpeed = 15f;
+    [SerializeField] private float stopWaitTime = 3f;
+    [Header("Stop Location")]
+    [SerializeField] private Transform stop;
+    private float distanceThreshold = 0.1f;
+    private Transform currentWaypoint;
     private Quaternion rotationGoal;
-    private Vector3 directionTowardWaypoint;
+    private Vector3 directionToWaypoint;
+    private bool isAtAStop = false;
 
-    
+
+
     void Start()
     {
-        SetupDirection();
-    
-    }
-
-    
+        SetPositionToStart();
+    }   
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
-        RotateTowardsCurrentWaypoint();
-        ContinueNextWayPoint();
+        if(!isAtAStop)
+        {
+            Moving();
+        }        
+    }    
+
+    private void SetPositionToStart()
+    {
+        //set initial position
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+        transform.position = currentWaypoint.position;
+
+        //set next waypoint
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+        transform.LookAt(currentWaypoint);
     }
 
-    private void ContinueNextWayPoint()
+    private void Moving()
     {
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, movementSpeed * Time.deltaTime);
         if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold)
         {
-            currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
+            if(currentWaypoint == stop)
+            {
+                StartCoroutine(TrainStop(stopWaitTime));    
+            }
+            else
+            {
+                currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+            }
+                 
         }
+        RotateTowardsWaypoint();
     }
 
-    void SetupDirection()
+    private void RotateTowardsWaypoint()
     {
-        currentWaypoint = waypoints.GetNextWayPoint(currentWaypoint);
-        transform.position = currentWaypoint.position;
+        directionToWaypoint = (currentWaypoint.position-transform.position).normalized;
+        rotationGoal = Quaternion.LookRotation(directionToWaypoint);
+        transform.rotation = Quaternion.Slerp(transform.rotation,rotationGoal, rotationSpeed * Time.deltaTime);
     }
 
-    void RotateTowardsCurrentWaypoint()
+    IEnumerator TrainStop(float seconds)
     {
-        directionTowardWaypoint = (currentWaypoint.position - transform.position).normalized;
-        rotationGoal = Quaternion.LookRotation(directionTowardWaypoint);
-
-        // Use RotateTowards instead of Slerp for smoother rotation
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationGoal, rotateSpeed * Time.deltaTime);
+        isAtAStop = true;
+        yield return new WaitForSeconds(seconds);
+        isAtAStop = false;
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
     }
 }
--- Jolly Season/Assets/Scripts/WayPoints/WayPoints.cs
+++ Jolly Season/Assets/Scripts/WayPoints/WayPoints.cs
@@ -4,71 +4,38 @@ using UnityEngine;
 
 public class Waypoints : MonoBehaviour
 {
-    [Range(0f,2f)]
-    [SerializeField] float waypointSize = 1f;
-    [Header("Path Settings")]
-    [SerializeField] private bool canLoop = true;
-    [SerializeField] private bool isMovingForward = true;
-    void OnDrawGizmos()
+    [Range(1,5)]
+    [SerializeField] private float gizmosSize = 2f;
+
+
+   void OnDrawGizmos()
+   {
+    foreach(Transform t in transform)
     {
-        foreach (Transform t in transform)
-        {
-            Gizmos.color = Color.blue;
-            Gizmos.DrawWireSphere(t.position,waypointSize);
-        }
-        Gizmos.color = Color.red;
-        for (int i =0; i <transform.childCount -1;i++)
-        {
-            Gizmos.DrawLine(transform.GetChild(i).position,transform.GetChild(i+1).position);
-            
-        }
-        if(canLoop)
-        {
-            Gizmos.DrawLine(transform.GetChild(transform.childCount-1).position, transform.GetChild(0).position);
-        }
-        
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(t.position,gizmosSize);
     }
-
-    public Transform GetNextWayPoint(Transform currentWayPoint)
+    Gizmos.color = Color.red;
+    for(int i = 0; i <transform.childCount -1; i++)
     {
-        if(currentWayPoint == null)
-        {
-            return transform.GetChild(0);
-        }
+        Gizmos.DrawLine(transform.GetChild(i).position,transform.GetChild(i +1).position);
+    }
+    Gizmos.DrawLine(transform.GetChild(transform.childCount - 1).position,transform.GetChild(0).position);
+   }
 
-        int currentIndex = currentWayPoint.GetSiblingIndex();
-
-        int nextIndex = currentIndex;
-
-        if(isMovingForward)
-        {
-            nextIndex+=1;
-            if(nextIndex == transform.childCount)
-            {
-                if(canLoop)
-                {
-                    nextIndex = 0;
-                }
-                if(!canLoop){
-                    nextIndex -=1;
-                }
-            }
-        }
-        else
-        {
-            nextIndex--;
-
-            if(nextIndex < 0)
-            {
-                if(canLoop)
-                {
-                    nextIndex = transform.childCount -1;
-                }
-                else {
-                    nextIndex ++;
-                }
-            }
-        }
-        return transform.GetChild(nextIndex);         
-    }      
+   public Transform GetNextWaypoint(Transform currentWaypoint)
+   {
+    if(currentWaypoint == null)
+    {
+        return transform.GetChild(0);
+    }
+    if(currentWaypoint.GetSiblingIndex() < transform.childCount - 1)
+    {
+        return transform.GetChild(currentWaypoint.GetSiblingIndex() + 1);
+    }
+    else
+    {
+        return transform.GetChild(0);
+    }
+   }
 }
